5b8fb88c1d03b26e36aedd0719b00a283546cfb2
AbdullahMukadam <abdullahmukadam21@gmail.com>
2026-09-08 06:30:15 -0700

    chore(version): bump to 0.1.1

--- apps/cli/src/constants.ts
+++ apps/cli/src/constants.ts
@@ -42,7 +42,7 @@ export function getDefaultConfig(): ProjectConfig {
 
 export const DEFAULT_CONFIG = getDefaultConfig();
 
-export const CLI_VERSION = "0.1.0";
+export const CLI_VERSION = "0.1.1";
 
 export const LANGUAGE_LABELS = {
   python: "Python",
